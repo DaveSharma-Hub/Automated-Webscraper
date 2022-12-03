b7fb9ffeacf0264987b9c3b768f64f830aa109ed
DaveSharma-Hub <davesharma12345@outlook.com>
2022-12-03 10:56:02 -0700

    Frontend and backend

---
+++ backend/main.py
@@ -0,0 +1,59 @@
+import requests
+from flask import Flask, redirect, url_for, render_template, request
+from bs4 import BeautifulSoup
+from flask_cors import CORS, cross_origin
+
+HEADERS = {
+    'User-Agent': ('Mozilla/5.0 (X11; Linux x86_64)'
+                    'AppleWebKit/537.36 (KHTML, like Gecko)'
+                    'Chrome/44.0.2403.157 Safari/537.36'),
+    'Accept-Language': 'en-US, en;q=0.5'
+}
+
+app = Flask(__name__)
+cors = CORS(app)
+
+# app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+website =[]
+
+def getWebsiteData(url,htmlElement,htmlIdentifier, identifierTag):
+    page = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(page.content,"html.parser")
+    return soup.find(htmlElement,{htmlIdentifier:identifierTag}).text.strip()
+
+def returnAllWebsiteData():
+    tmpArray = []
+    for i in website:
+        webData = getWebsiteData(str(i['website']),str(i['html']),str(i['id']),str(i['tag']))
+        tmpArray.append({
+            'website':i['website'],
+            'data':webData
+        })
+    return tmpArray
+
+@app.route('/addWebsite', methods=['POST'])
+@cross_origin()
+def postWebsite():
+    request_data = request.get_json()
+    website.append({
+        'website':request_data['website'],
+        'html':request_data['html'],
+        'id':request_data['id'],
+        'tag':request_data['tag']
+    })
+    print(website)
+    data = returnAllWebsiteData()
+    print(data)
+    return data
+
+@app.route('/getWebsites')
+@cross_origin()
+def getWebsite():
+    return returnAllWebsiteData()
+
+if __name__ == '__main__':
+    print(getWebsiteData('https://www.npmjs.com/package/axios','span','class', '_76473bea f6 dib ph0 pv2 mb2-ns black-80 nowrap f5 fw4 lh-copy'))
+    app.run('0.0.0.0',port=5000)
+
---
+++ web-scraper/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import axios from 'axios'
+import './App.scss'
+import { useEffect } from 'react'
+
+function App() {
+  const [websiteList, setWebsiteList]= useState([])
+  const [website, setWebsite] = useState('')
+  const [htmlElement, setHtmlElement]= useState('')
+  const [ids, setIds]= useState('')
+  const [idTag, setIdTag]= useState('')
+
+  const handleAddWebsite = () =>{
+    axios({
+      method: 'post',
+      url: 'http://localhost:5000/addWebsite',
+      data: {
+        website: website,
+        html: htmlElement,
+        id:ids,
+        tag:idTag
+      }
+    }).then(function (response) {
+      setWebsiteList(response.data)
+      setWebsite('')
+      setHtmlElement('')
+      setIds('')
+      setIdTag('')
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert('Try again')
+    });
+  }
+
+  const handleGetAllWebsites = () =>{
+    axios({
+      method: 'get',
+      url: 'http://localhost:5000/getWebsites',
+    }).then(function (response) {
+      setWebsiteList(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  useEffect(()=>{
+    handleGetAllWebsites()
+  },[])
+
+  return (
+    <div className="mainApp">
+      <div className='AppForm'>
+      <h3>Webscrapio</h3>
+        <form
+          onSubmit={(e)=>{
+            e.preventDefault();
+            handleAddWebsite()
+          }}
+        >
+          <input placeholder='Website' value={website} onChange={(e)=>{setWebsite(e.target.value)}}/>
+          <div className='AppIdentifierTypes'>
+            <input placeholder='HTML element(eg.div,p,h1,etc)' value={htmlElement} onChange={(e)=>{setHtmlElement(e.target.value)}}/>
+            <input placeholder='Identifier(eg.class,id,custom-id,etc)' value={ids} onChange={(e)=>{setIds(e.target.value)}}/>
+            <input placeholder='Identifier tag(eg.5 where id=5)' value={idTag} onChange={(e)=>{setIdTag(e.target.value)}}/>
+          </div>
+          <button>Add</button>
+        </form>
+      </div>
+          {
+            websiteList?.map((website)=>{
+              return(
+                <div className='displayWebsite'>
+                  <div>
+                    <h4>{website.website}</h4>
+                    <p>Data: {website.data}</p>
+                  </div>
+                </div>
+              )
+            })
+          }
+    </div>
+  )
+}
+
+export default App
